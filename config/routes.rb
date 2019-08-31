4f7a323bb021cf642b2cf6429eca2f5a0d380a94
salman778 <alk-333@hotmail.com>
2019-08-31 14:18:30 -0700

    add all routes for customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :admins, controllers: { registrations: 'admins/registrations',
-                                     sessions: 'admins/sessions', unlocks: 'admins/unlocks',
-                                     passwords: 'admins/passwords', confirmations: 'admins/confirmations'}
+  devise_for :admins, controllers: {  registrations: 'admins/registrations', sessions: 'admins/sessions', unlocks: 'admins/unlocks', passwords: 'admins/passwords', 
+                                      confirmations: 'admins/confirmations'}
+
+  devise_for :customers, controllers: { registrations: 'customers/registrations', sessions: 'customers/sessions', unlocks: 'customers/unlocks', 
+                                        passwords: 'customers/passwords', confirmations: 'customers/confirmations'}
 end
